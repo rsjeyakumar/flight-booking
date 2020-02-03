fd19f5f0eac4af0c166a09b8fa57291b559c6b14
krishna <krishnakuma@hcl.com>
2020-02-03 18:28:31 +0530

    booking page added

--- src/app/component/booking/booking.component.ts
+++ src/app/component/booking/booking.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
@@ -6,10 +7,76 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent implements OnInit {
+  dynamicForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  flightDeatils = {
+    flightName: 'Air Asia',
+    departureLocation: 'Bangalore',
+    arrivalLocation: 'Chennai',
+    departureDate: new Date(),
+    duration: new Date().getTime(),
+    departureTime: new Date(),
+    arrivalTime: new Date().getTime(),
+    price: 2000,
+    travelId: 2,
+    flightId: 45
+  };
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.dynamicForm = this.formBuilder.group({
+      tickets: new FormArray([])
+    });
+
+    this.onChangeTickets(2);
+  }
+
+  // convenience getters for easy access to form fields
+  get f() { return this.dynamicForm.controls; }
+  get t() { return this.f.tickets as FormArray; }
+
+  onChangeTickets(e) {
+    const numberOfTickets = e || 0;
+    if (this.t.length < numberOfTickets) {
+      for (let i = this.t.length; i < numberOfTickets; i++) {
+        this.t.push(this.formBuilder.group({
+          passengerName: ['', Validators.required],
+          passengerAge: ['', [Validators.required, Validators.email]]
+        }));
+      }
+    } else {
+      for (let i = this.t.length; i >= numberOfTickets; i--) {
+        this.t.removeAt(i);
+      }
+    }
+  }
+
+  onSubmit() {
+    alert('test');
+    this.submitted = true;
+
+    // stop here if form is invalid
+    // if (this.dynamicForm.invalid) {
+    //   return;
+    // }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.dynamicForm.value, null, 4));
+  }
+
+  onReset() {
+    // reset whole form back to initial state
+    this.submitted = false;
+    this.dynamicForm.reset();
+    this.t.clear();
+  }
+
+  onClear() {
+    // clear errors and reset ticket fields
+    this.submitted = false;
+    this.t.reset();
   }
 
 }
