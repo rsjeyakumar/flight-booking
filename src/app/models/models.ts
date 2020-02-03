965120c37aab6beb4e8851ad9ab77d9eadc6a683
User1 <User1@LP-5CD9296CM7.HCLT.CORP.HCL.IN>
2020-02-03 18:26:05 +0530

    Booking Grid with Navigation

---
+++ src/app/models/models.ts
@@ -0,0 +1,12 @@
+export interface Search {
+    flightName: string;
+    departureLocation: string;
+    arrivalLocation: string;
+    departureDate: Date;
+    duration: Date;
+    departureTime: Date;
+    arrivalTime: Date;
+    price: number;
+    travelId: number;
+    flightId: number;
+}
