69d7967371edd2744f4b6e79be77854f46306c09
pradeep Kumar <pradeep@gmail.com>
2020-02-04 15:02:32 +0530

    added reload issue

--- src/app/component/cancel/cancel.component.ts
+++ src/app/component/cancel/cancel.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FlightBookingService } from '../../services/flight-booking.service';
-
+import swal from 'sweetalert';
 @Component({
   selector: 'app-cancel',
   templateUrl: './cancel.component.html',
